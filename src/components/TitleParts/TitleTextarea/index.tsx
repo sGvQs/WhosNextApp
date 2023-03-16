1db95ed98eb8f49d7f93f32f8a49570fad17a009
sGvQs <susum.earth@gmail.com>
2023-03-16 19:09:20 +0900

    コマンド入力で名前が出力されるように変更

--- src/components/TitleParts/TitleTextarea/index.tsx
+++ src/components/TitleParts/TitleTextarea/index.tsx
@@ -5,6 +5,43 @@ import { StyledTextarea } from './styled';
 export const TitleTextArea: React.VFC = () => {
   const { nameList, setNameList } = useStoreState();
   const [currentValue, setCurrentValue] = React.useState<string>();
+
+  const onKeyPress = () => {
+    if (!currentValue) return;
+    if (!nameList) {
+      setNameList([currentValue.replace(/\n/g, '')]);
+      setCurrentValue('');
+    } else {
+      setNameList([...nameList, currentValue.replace(/\n/g, '')]);
+      setCurrentValue('');
+    }
+  };
+
+  const kermit = ['goto', 'saito', 'nakatsuka', 'nishihara', 'hirai'];
+  const rosita = ['sato', 'watanabe', 'kuwata', 'goshima', 'kaino'];
+
+  React.useEffect(() => {
+    if (currentValue !== 'kermit-dev') return;
+    if (!nameList) {
+      setNameList(kermit);
+      setCurrentValue('');
+    } else {
+      setNameList([...nameList, ...kermit]);
+      setCurrentValue('');
+    }
+  }, [currentValue]);
+
+  React.useEffect(() => {
+    if (currentValue !== 'rosita-dev') return;
+    if (!nameList) {
+      setNameList(rosita);
+      setCurrentValue('');
+    } else {
+      setNameList([...nameList, ...rosita]);
+      setCurrentValue('');
+    }
+  }, [currentValue]);
+
   return (
     <StyledTextarea
       value={currentValue}
@@ -12,14 +49,7 @@ export const TitleTextArea: React.VFC = () => {
       onChange={(e) => setCurrentValue(e.target.value.replace(/\n/g, ''))}
       onKeyPress={(e) => {
         if (e.key !== 'Enter') return;
-        if (!currentValue) return;
-        if (!nameList) {
-          setNameList([currentValue.replace(/\n/g, '')]);
-          setCurrentValue('');
-        } else {
-          setNameList([...nameList, currentValue.replace(/\n/g, '')]);
-          setCurrentValue('');
-        }
+        onKeyPress();
       }}
     />
   );
